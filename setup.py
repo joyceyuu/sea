3db8689f1d6eda0f8dcf738793e0fd7d94506baa
Joyce Wang <joyce.wang@data61.csiro.au>
2018-03-13 22:36:03 +1100

    Sort dependency

--- setup.py
+++ setup.py
@@ -3,57 +3,60 @@
 
 from setuptools import setup, find_packages
 
-exec(open('sea/__version__.py').read())
-readme = open('README.md').read()
+exec(open("sea/__version__.py").read())
+readme = open("README.md").read()
 
 setup(
-    name='sea',
-    version='0.1.0',
-    description='A Python Package',
+    name="sea",
+    version="0.1.0",
+    description="A Python Package",
     long_description=readme,
-    author='sea',
-    author_email='joyce.xinyue.wang@gmail.com',
-    url='https://github.com/JoyceXinyueWang/sea',
+    author="sea",
+    author_email="joyce.xinyue.wang@gmail.com",
+    url="https://github.com/JoyceXinyueWang/sea",
     packages=find_packages(),
-    package_dir={'sea': 'sea'},
+    package_dir={"sea": "sea"},
     include_package_data=True,
     entry_points={
-        'console_scripts': [
-            'sea = sea.scripts.cli:cli',
-        ]
-    },
+        "console_scripts": [
+            "sea = sea.scripts.cli:cli",
+            ]
+        },
     install_requires=[
-        'numpy==1.13.1',
-        'scipy==0.19.1',
-        'click==6.7',
-        'mypy==0.521',
-        'mypy_extensions==0.3.0',
-        'modron==0.1.0',
-        'dretch==0.1.0',
-        'glabrezu==0.3.0',
-    ],
-    dependency_links=[
-        'git+ssh://git@github.com/determinant-io/dretch.git@refactor#egg=dretch-0.1.0',
-        'git+ssh://git@github.com/determinant-io/modron.git@develop#egg=modron-0.1.0',
-        'git+ssh://git@github.com/determinant-io/glabrezu.git@develop#egg=glabrezu-0.3.0',
-    ],
+        "numpy==1.13.1",
+        "scipy==0.19.1",
+        "click==6.7",
+        "pandas==0.22.0",
+        "aboleth==0.7.0",
+        "sklearn==0.0",
+        # "mypy==0.521",
+        # "mypy_extensions==0.3.0",
+        # "modron==0.1.0",
+        # "dretch==0.1.0",
+        # "glabrezu==0.3.0",
+        ],
+    # dependency_links=[
+    #     "git+ssh://git@github.com/determinant-io/dretch.git@refactor#egg=dretch-0.1.0",
+    #     "git+ssh://git@github.com/determinant-io/modron.git@develop#egg=modron-0.1.0",
+    #     "git+ssh://git@github.com/determinant-io/glabrezu.git@develop#egg=glabrezu-0.3.0",
+    # ],
     extras_require={
-        'dev': [
-            'pytest>=3.1.3',
-            'pytest-flake8>=0.8.1',
-            'pytest-mock>=1.6.2',
-            'pytest-cov>=2.5.1',
-            'pytest-regtest>=0.15.1',
-            'flake8-docstrings>=1.1.0',
-            'flake8-quotes>=0.11.0',
-            'flake8-comprehensions>=1.4.1'
-        ]
-    },
+        "dev": [
+            "pytest>=3.1.3",
+            "pytest-flake8>=0.8.1",
+            "pytest-mock>=1.6.2",
+            "pytest-cov>=2.5.1",
+            "pytest-regtest>=0.15.1",
+            "flake8-docstrings>=1.1.0",
+            "flake8-quotes>=0.11.0",
+            "flake8-comprehensions>=1.4.1"
+            ]
+        },
     license="All Rights Reserved",
     zip_safe=False,
-    keywords='sea',
+    keywords="sea",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        "Development Status :: 2 - Pre-Alpha",
         "Operating System :: POSIX",
         "Natural Language :: English",
         "Programming Language :: Python",
@@ -61,5 +64,5 @@ setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+        ],
+    )
